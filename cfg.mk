c4398cd51bd022966896f160a7e5f27775b87560
Pádraig Brady <P@draigBrady.com>
2026-06-05 17:57:25 +0100

    maint: avoid sc_require_config_h failure
    * cfg.mk: Exclude make_buz_table from requiring config.h.

--- cfg.mk
+++ cfg.mk
@@ -933,7 +933,8 @@ exclude_file_name_regexp--sc_system_h_headers = \
   ^src/($(_x_system_h)|$(_x_system_c))$$
 
 _src := (false|lbracket|chown-(chgrp|chown)
-_src := $(_src)|ls-(dir|ls|vdir)|make-prime-list|tac-pipe|uname-(arch|uname))
+_src := $(_src)|ls-(dir|ls|vdir)|make-prime-list|make-buz-table
+_src := $(_src)|tac-pipe|uname-(arch|uname))
 _gl_src = (xdecto.max|cl-strtold)
 exclude_file_name_regexp--sc_require_config_h_first = \
   (^lib/buffer-lcm\.c|gl/lib/$(_gl_src)\.c|src/$(_src)\.c)$$
